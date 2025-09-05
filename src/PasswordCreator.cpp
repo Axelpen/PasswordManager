bfe7e3ef730e3e1925108dfebbd2357face84d5e
Alexander Penck <Alexander.Penck@ncad.de>
2025-09-05 12:30:09 +0200

    PasswordCreator

--- src/PasswordCreator.cpp
+++ src/PasswordCreator.cpp
@@ -1,4 +1,5 @@
 #include "PasswordCreator.h"
+#include <random>
 #include <string>
 
 
@@ -30,12 +31,36 @@ PasswordCreator::PasswordType PasswordCreator::passwordSecurity() {
   return type;
 }
 
-std::string PasswordCreator::createPassword(PasswordType type){
-  switch (type) {
-    case(Alphabet):
-      break;
-    default:
-        
-      break;
-  }
+std::string PasswordCreator::createPassword(PasswordType type) {
+    const std::string letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const std::string numbers = "0123456789";
+    const std::string symbols = "!@#$%^&*()-_=+[]{}<>?/|";
+
+    std::string charset;
+
+    switch (type) {
+        case Alphabet:
+            charset = letters;
+            break;
+        case AlphabetNumber:
+            charset = letters + numbers;
+            break;
+        case AlphabetNumberSymbol:
+            charset = letters + numbers + symbols;
+            break;
+        default:
+            std::cerr << "Invalid password type selected." << std::endl;
+            return "";
+    }
+
+    const int passwordLength = 12; // or ask the user for length
+    std::string password;
+    std::mt19937 rng(static_cast<unsigned int>(std::time(nullptr))); // seed
+    std::uniform_int_distribution<> dist(0, static_cast<int>(charset.size() - 1));
+
+    for (int i = 0; i < passwordLength; ++i) {
+        password += charset[dist(rng)];
+    }
+
+    return password;
 }
